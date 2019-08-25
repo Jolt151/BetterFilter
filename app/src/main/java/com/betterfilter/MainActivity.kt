b654b03f0acae6a2250265221de1f165265f61c8
Michael Levi <michaellevi151@gmail.com>
2019-08-25 13:34:20 -0400

    Add code for downloading files to cache

--- app/src/main/java/com/betterfilter/MainActivity.kt
+++ app/src/main/java/com/betterfilter/MainActivity.kt
@@ -2,11 +2,49 @@ package com.betterfilter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.error
+import org.jetbrains.anko.info
+import java.io.*
+import android.R.attr.data
 
-class MainActivity : AppCompatActivity() {
+
+
+
+
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val downloadHostsButton: Button = find(R.id.downloadHosts)
+        downloadHostsButton.setOnClickListener {
+            info("clicked")
+
+            doAsync(exceptionHandler = {
+                error(it)
+            }) {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url("https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts")
+                    .build()
+                val response = client.newCall(request).execute()
+                val inputStream: InputStream = response.body?.byteStream() ?: throw Exception("null body")
+
+                val file = File(cacheDir, "hosts")
+                file.delete()
+                info(file.absolutePath)
+                val fos = FileOutputStream(file)
+                fos.write(inputStream.readBytes())
+
+            }
+        }
+
     }
 }

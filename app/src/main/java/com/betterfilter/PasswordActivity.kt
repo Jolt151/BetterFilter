cbac735901829731f5acfc5c4c588ca83d5ed388
Michael Levi <michaellevi151@gmail.com>
2019-08-25 23:16:07 -0400

    Add some utils to be used for hashing passwords

--- app/src/main/java/com/betterfilter/PasswordActivity.kt
+++ app/src/main/java/com/betterfilter/PasswordActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.betterfilter.Extensions.sha256
 import org.jetbrains.anko.find
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class PasswordActivity : AppCompatActivity() {
 
@@ -16,10 +19,11 @@ class PasswordActivity : AppCompatActivity() {
         val enterPasswordButton: Button = find(R.id.enterPasswordButton)
 
         enterPasswordButton.setOnClickListener {
-            if (passwordEditText.text.toString() == "1234") {
+            if (passwordEditText.text.toString().sha256() == "1234".sha256()) {
                 App.isAuthenticated = true
                 finish()
             }
+
         }
     }
 }

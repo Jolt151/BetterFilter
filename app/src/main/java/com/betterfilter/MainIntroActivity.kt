e21b80a10628a66634e274b66bca44a008576560
Michael Levi <michaellevi151@gmail.com>
2019-08-27 13:19:38 -0400

    Add a slide to enable device administrator

--- app/src/main/java/com/betterfilter/MainIntroActivity.kt
+++ app/src/main/java/com/betterfilter/MainIntroActivity.kt
@@ -1,6 +1,9 @@
 package com.betterfilter
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +16,6 @@ import com.github.paolorotolo.appintro.AppIntro
 import com.github.paolorotolo.appintro.ISlidePolicy
 import org.jetbrains.anko.support.v4.find
 import org.jetbrains.anko.support.v4.toast
-import org.jetbrains.anko.toast
 
 
 class MainIntroActivity : AppIntro() {
@@ -21,6 +23,7 @@ class MainIntroActivity : AppIntro() {
         super.onCreate(savedInstanceState)
 
         addSlide(WelcomeFragment())
+        addSlide(SetDeviceAdminFragment())
 
     }
 }
@@ -64,4 +67,48 @@ class WelcomeFragment: Fragment(), ISlidePolicy {
         toast("You need to set a password before continuing.")
     }
 
+}
+
+class SetDeviceAdminFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.intro_become_admin, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val setDeviceAdminButton: Button = find(R.id.setDeviceAdminButton)
+
+        val componentName = ComponentName(this.context, PolicyAdmin::class.java)
+        val devicePolicyManager = this.context?.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        if (devicePolicyManager.isAdminActive(componentName)) {
+            setDeviceAdminButton.text = "Enabled"
+            setDeviceAdminButton.isEnabled = false
+        }
+
+        setDeviceAdminButton.setOnClickListener {
+            if (!devicePolicyManager.isAdminActive(componentName)) {
+                val activateDeviceAdminIntent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+
+                activateDeviceAdminIntent.putExtra(
+                    DevicePolicyManager.EXTRA_DEVICE_ADMIN,
+                    ComponentName(this.context, PolicyAdmin::class.java)
+                )
+
+                // It is good practice to include the optional explanation text to
+                // explain to user why the application is requesting to be a device
+                // administrator. The system will display this message on the activation
+                // screen.
+                activateDeviceAdminIntent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Stop the filter from being uninstalled")
+
+                startActivityForResult(activateDeviceAdminIntent, 1234)
+            } else {
+                toast("Already enabled!")
+            }
+        }
+    }
 }

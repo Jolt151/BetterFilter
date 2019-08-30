3b69a1c4bb0cc8a16acd37fc46fcd9e6b88b6171
Michael Levi <michaellevi151@gmail.com>
2019-08-30 15:16:19 -0400

    test using fragment for selecting hosts sources, might not work

--- app/src/main/java/com/betterfilter/ChooseHostsSourcesActivity.kt
+++ app/src/main/java/com/betterfilter/ChooseHostsSourcesActivity.kt
@@ -11,15 +11,20 @@ import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.util.Log
 import android.widget.*
 import androidx.core.view.marginTop
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_choose_hosts_sources.*
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.longSnackbar
 import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.support.v4.alert
+import org.jetbrains.anko.support.v4.defaultSharedPreferences
+import org.jetbrains.anko.support.v4.find
+import org.jetbrains.anko.support.v4.toast
 import java.util.*
 import kotlin.collections.ArrayList
 
 
-class ChooseHostsSourcesActivity : AppCompatActivity(), AnkoLogger {
+class ChooseHostsSourcesActivity : Fragment(), AnkoLogger {
 
     lateinit var listView: ListView
     lateinit var arrayAdapter: HostsAdapter
@@ -28,24 +33,35 @@ class ChooseHostsSourcesActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_hosts_sources)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         hosts = defaultSharedPreferences.getStringSet("hosts-urls", mutableSetOf()) ?: mutableSetOf()
         hostsList = ArrayList(hosts)
-        listView = findViewById(R.id.listview)
-        arrayAdapter = HostsAdapter(this, ArrayList(hostsList.sorted()))
+        listView = find(R.id.listview)
+        arrayAdapter = HostsAdapter(requireContext(), ArrayList(hostsList.sorted()))
         listView.adapter = arrayAdapter
-
+        return inflater.inflate(R.layout.activity_choose_hosts_sources, container, false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        return inflater.inflate(R.menu.menu_settings_choose_hosts_sources, menu)
+    }
+
+/*    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         debug("oncreate options menu")
         getMenuInflater().inflate(R.menu.menu_settings_choose_hosts_sources, menu)
         return true
-    }
+    }*/
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        val id = item?.getItemId()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
         if (id == R.id.menu_add_host) {
             this.alert("Add custom source") {
                 lateinit var hostsEditText: EditText
@@ -89,6 +105,52 @@ class ChooseHostsSourcesActivity : AppCompatActivity(), AnkoLogger {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    /*override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.getItemId()
+        if (id == R.id.menu_add_host) {
+            this.alert("Add custom source") {
+                lateinit var hostsEditText: EditText
+                customView {
+                    verticalLayout {
+                        hostsEditText = editText {
+                            top
+                            hint = "Hosts file URL"
+                        }.lparams {
+                            topMargin = 10
+                            width = matchParent
+                        }
+                    }
+                }
+                yesButton {
+
+                    if (hostsEditText.text.isNotBlank()) {
+                        //add to sharedPreferences
+                        val hostsSet: MutableSet<String> = defaultSharedPreferences.getStringSet("hosts-urls", mutableSetOf()) ?: mutableSetOf()
+                        hostsSet.add(hostsEditText.text.toString())
+                        with(defaultSharedPreferences.edit()) {
+                            //for some reason, we need to remove the set and apply first or it doesn't work
+                            //possibly something to do with the memory references
+                            //see https://stackoverflow.com/questions/17469583/setstring-in-android-sharedpreferences-does-not-save-on-force-close
+                            remove("hosts-urls")
+                            apply()
+                            putStringSet("hosts-urls", hostsSet)
+                            apply()
+                        }
+                        arrayAdapter = HostsAdapter(this.ctx, ArrayList(hostsSet))
+                        listView.adapter = arrayAdapter
+                    }
+                }
+                noButton {
+                    it.dismiss()
+                }
+            }.show()
+            return true
+        } else if (id == R.id.menu_info_hosts) {
+            //show some information about hosts files
+        }
+        return super.onOptionsItemSelected(item)
+    }*/
 
     inner class HostsAdapter(private val context: Context,
                         private val dataSource: ArrayList<String>) : BaseAdapter() {
